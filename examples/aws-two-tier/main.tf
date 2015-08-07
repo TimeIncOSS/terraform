0ffe030c88155bec1f5541868c8650818eb55ed5
Radek Simko <radek.simko@timeinc.com>
2015-08-07 12:00:22 +0100

    Update two-tier AWS example ++security

--- examples/aws-two-tier/main.tf
+++ examples/aws-two-tier/main.tf
@@ -5,33 +5,46 @@ provider "aws" {
 
 # Our default security group to access
 # the instances over SSH and HTTP
-resource "aws_security_group" "default" {
-    name = "terraform_example"
-    description = "Used in the terraform"
+resource "aws_security_group" "instance" {
+  name = "terraform_example_instance"
+  description = "Used in the terraform"
 
-    # SSH access from anywhere
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # HTTP access from anywhere
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      security_groups = ["${aws_security_group.lb.id}"]
+  }
 
-    # HTTP access from anywhere
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # outbound internet access
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-    # outbound internet access
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+resource "aws_security_group" "lb" {
+  name = "terraform_example_lb"
+  description = "Used in the terraform"
+
+  # HTTP access from anywhere
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
@@ -48,21 +61,23 @@ resource "aws_elb" "web" {
     lb_protocol = "http"
   }
 
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:80/"
+    interval = 5
+  }
+
   # The instance is registered automatically
   instances = ["${aws_instance.web.id}"]
+
+  security_groups = ["${aws_security_group.lb.id}"]
 }
 
 
 resource "aws_instance" "web" {
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
-  connection {
-    # The default username for our AMI
-    user = "ubuntu"
-
-    # The path to your keyfile
-    key_file = "${var.key_path}"
-  }
+  associate_public_ip_address = false
 
   instance_type = "m1.small"
 
@@ -78,16 +93,15 @@ resource "aws_instance" "web" {
   key_name = "${var.key_name}"
 
   # Our Security group to allow HTTP and SSH access
-  security_groups = ["${aws_security_group.default.name}"]
+  security_groups = ["${aws_security_group.instance.name}"]
 
   # We run a remote provisioner on the instance after creating it.
   # In this case, we just install nginx and start it. By default,
   # this should be on port 80
-  provisioner "remote-exec" {
-    inline = [
-        "sudo apt-get -y update",
-        "sudo apt-get -y install nginx",
-        "sudo service nginx start"
-    ]
-  }
+  user_data = <<USER_DATA
+#!/bin/bash
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+USER_DATA
 }
---
+++ examples/aws-two-tier/terraform.tfvars
@@ -0,0 +1,2 @@
+key_name = "coreos-test"
+key_path = "~/.ssh/coreos-test-dam_us-west-2.pem"
